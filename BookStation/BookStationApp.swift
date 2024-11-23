32d848d3b6f36e51ff104425f05b36d3305dc7f9
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-11-22 21:10:40 -0600

    Project init

---
+++ BookStation/BookStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStationApp.swift
+//  BookStation
+//
+//  Created by TECNOSYSTEM2000 on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BookStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookStation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookStation
+//
+//  Created by TECNOSYSTEM2000 on 22/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
